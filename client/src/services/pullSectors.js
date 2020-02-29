9da73b7d4fd8b9c542496d025c4ef19373b8b7a6
Logan Mc <logangmcnulty@gmail.com>
2020-02-29 16:01:17 -0500

    cron job saturday test

--- client/src/services/pullSectors.js
+++ client/src/services/pullSectors.js
@@ -8,106 +8,105 @@ var CronJob = require('cron').CronJob;
 export function updateSectorData() {
 
   // cron job runs every Friday at 18:00
-  const job = new CronJob('*/1 * * * *', function() {
-    console.log("HELLO CRON")
-  //   const apiKey = 'EG1B4JUOHK5U6LNR'
-  //   let counter = 0
-  //   let thisStockData = {
-  //     name: testsData[counter].Company,
-  //     symbol: testsData[counter].Stock,
-  //     topHoldingsNames: [],
-  //     topHoldingsPcts: [],
-  //     priceData: [],
-  //     adxData: [],
-  //     macdData: []
-  //   }
+  const job = new CronJob('10 16 * * 6', function() {
+    console.log("CRON JOB UPDATING SECTOR DATA")
+    const apiKey = 'EG1B4JUOHK5U6LNR'
+    let counter = 0
+    let thisStockData = {
+      name: testsData[counter].Company,
+      symbol: testsData[counter].Stock,
+      topHoldingsNames: [],
+      topHoldingsPcts: [],
+      priceData: [],
+      adxData: [],
+      macdData: []
+    }
 
-  // //kick off
-  // const getAdxData = () => {
-  //   $.ajax({
-  //     /* The whisperingforest.org URL is not longer valid, I found a new one that is similar... */
-  //     url: `https://www.alphavantage.co/query?function=ADX&symbol=${testsData[counter].Stock}&interval=weekly&time_period=10&apikey=${apiKey}`,
-  //     async: true,
-  //     dataType: 'json',
-  //     success: function(res) {
-  //       Object.entries(res['Technical Analysis: ADX']).forEach(
-  //         ([key, value], index) => {
-  //           thisStockData.adxData.push(Number(value['ADX']))
-  //         }
-  //       )
-  //       getTopHoldings()
-  //     }
-  //   })
-  // }
+  //kick off
+  const getAdxData = () => {
+    $.ajax({
+      /* The whisperingforest.org URL is not longer valid, I found a new one that is similar... */
+      url: `https://www.alphavantage.co/query?function=ADX&symbol=${testsData[counter].Stock}&interval=weekly&time_period=10&apikey=${apiKey}`,
+      async: true,
+      dataType: 'json',
+      success: function(res) {
+        Object.entries(res['Technical Analysis: ADX']).forEach(
+          ([key, value], index) => {
+            thisStockData.adxData.push(Number(value['ADX']))
+          }
+        )
+        getTopHoldings()
+      }
+    })
+  }
 
-  // const getTopHoldings = () => {
-  //   console.log("Yahoo data...")
-  //   yahooDataPull(thisStockData.symbol).then((yahooData) => {
-  //     for (let i = 0; i < 10; i++){
-  //       !yahooData.data.defaultKeyStatistics.err ? thisStockData.topHoldingsNames.push(yahooData.data.topHoldings.holdings[i].symbol) : thisStockData.topHoldingsNames.push("")
-  //       !yahooData.data.defaultKeyStatistics.err ? thisStockData.topHoldingsPcts.push(yahooData.data.topHoldings.holdings[i].holdingPercent.raw) : thisStockData.topHoldingsPcts.push("")
-  //     }
-  //   }).then(()=> {
-  //     getMacdData()
-  //   })
-  // }
+  const getTopHoldings = () => {
+    console.log("Yahoo data...")
+    yahooDataPull(thisStockData.symbol).then((yahooData) => {
+      for (let i = 0; i < 10; i++){
+        !yahooData.data.defaultKeyStatistics.err ? thisStockData.topHoldingsNames.push(yahooData.data.topHoldings.holdings[i].symbol) : thisStockData.topHoldingsNames.push("")
+        !yahooData.data.defaultKeyStatistics.err ? thisStockData.topHoldingsPcts.push(yahooData.data.topHoldings.holdings[i].holdingPercent.raw) : thisStockData.topHoldingsPcts.push("")
+      }
+    }).then(()=> {
+      getMacdData()
+    })
+  }
 
-  // const getMacdData = () => {
-  //   $.ajax({
-  //     /* The whisperingforest.org URL is not longer valid, I found a new one that is similar... */
-  //     url: `https://www.alphavantage.co/query?function=MACD&symbol=${testsData[counter].Stock}&interval=weekly&series_type=close&apikey=${apiKey}`,
-  //     async: true,
-  //     dataType: 'json',
-  //     success: function(res) {
-  //       Object.entries(res['Technical Analysis: MACD']).forEach(
-  //         ([key, value], index) => {
-  //           thisStockData.macdData.push(Number(value['MACD']))
-  //         }
-  //       )
+  const getMacdData = () => {
+    $.ajax({
+      /* The whisperingforest.org URL is not longer valid, I found a new one that is similar... */
+      url: `https://www.alphavantage.co/query?function=MACD&symbol=${testsData[counter].Stock}&interval=weekly&series_type=close&apikey=${apiKey}`,
+      async: true,
+      dataType: 'json',
+      success: function(res) {
+        Object.entries(res['Technical Analysis: MACD']).forEach(
+          ([key, value], index) => {
+            thisStockData.macdData.push(Number(value['MACD']))
+          }
+        )
         
-  //       console.log("PASSING WASHED SECTOR TO Update sectors API")
-  //       console.log(thisStockData)
+        console.log("PASSING WASHED SECTOR TO Update sectors API")
+        console.log(thisStockData)
 
-  //       let apiEndpoint = apiUrl + '/updateSectors'
-  //       http.put(apiEndpoint, thisStockData)
-  //       counter++
+        let apiEndpoint = apiUrl + '/updateSectors'
+        http.put(apiEndpoint, thisStockData)
+        counter++
         
-  //       if (counter < testsData.length) {
-  //         getPriceData()
-  //         thisStockData = {
-  //           name: testsData[counter].Company,
-  //           symbol: testsData[counter].Stock,
-  //           priceData: [],
-  //           adxData: [],
-  //           macdData: [],
-  //           topHoldingsNames: [],
-  //           topHoldingsPcts: []
-  //         }
-  //       }
-  //     }
-  //   })
-  // }
+        if (counter < testsData.length) {
+          getPriceData()
+          thisStockData = {
+            name: testsData[counter].Company,
+            symbol: testsData[counter].Stock,
+            priceData: [],
+            adxData: [],
+            macdData: [],
+            topHoldingsNames: [],
+            topHoldingsPcts: []
+          }
+        }
+      }
+    })
+  }
 
-  // const getPriceData = () => {
-  //   //price data first
-  //   $.ajax({
-  //     url: `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${testsData[counter].Stock}&apikey=${apiKey}`,
-  //     async: true,
-  //     dataType: 'json',
-  //     success: function(res) {
-  //       Object.entries(res['Weekly Time Series']).forEach(
-  //         ([key, value], index) => {
-  //           thisStockData.priceData.push(Number(value['4. close']))
-  //         }
-  //       )
-  //       getAdxData()
-  //     }
-  //   })
-  // }
+  const getPriceData = () => {
+    //price data first
+    $.ajax({
+      url: `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${testsData[counter].Stock}&apikey=${apiKey}`,
+      async: true,
+      dataType: 'json',
+      success: function(res) {
+        Object.entries(res['Weekly Time Series']).forEach(
+          ([key, value], index) => {
+            thisStockData.priceData.push(Number(value['4. close']))
+          }
+        )
+        getAdxData()
+      }
+    })
+  }
 
-  // getPriceData()
+  getPriceData()
 
-//commenting out cron job for now for Dev purposes
   });
   job.start();
 }
